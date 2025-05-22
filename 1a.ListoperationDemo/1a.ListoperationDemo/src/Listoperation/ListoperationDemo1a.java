c1466c1a083cdf06ec498047bf1311a6d0795763
SHREYA GAONKAR <154246220+shreyagaonkar28@users.noreply.github.com>
2025-05-22 20:06:43 +0530

    Update ListoperationDemo1a.java

--- 1a.ListoperationDemo/1a.ListoperationDemo/src/Listoperation/ListoperationDemo1a.java
+++ 1a.ListoperationDemo/1a.ListoperationDemo/src/Listoperation/ListoperationDemo1a.java
@@ -1,3 +1,5 @@
+//1.a Develop a Java program for adding elements [Apple, Banana, Orange] into an ArrayList and a LinkedList to perform the following operations with different functions directed asfollows
+//1. Adding elements, 2. Adding element at specific index, 3. Adding multiple elements, 4.Accessing elements,5. Updating elements, 6. Removing elements, 7. Searching elements, 8. List size, 9. Iterating over list, 10. Using Iterator, 11. Sorting, 12. Sublist, 13. Clearing the list
 package Listoperation;
 import java.util.*;
 public class ListoperationDemo1a {
